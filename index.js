b51eef6e6dbfad8b066baa016010e35148d28699
Adam Cholmondeley <sovchickendipps@gmail.com>
2024-02-06 14:21:53 +0000

    Added a basic timer and updated readme

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Your code goes here
+
+
+let intervalId;
+
+//Checks if the start button is clicked, if so, it starts the timer
+function startButtonAttacher() {
+    let button = document.querySelector("#start");
+    button.addEventListener("click", () => {
+        intervalId = setInterval(() => {
+            let input = document.querySelector("#timer").value;
+            let inputArray = input.split(":");
+            let minutes = parseInt(inputArray[0]);
+            let seconds = parseInt(inputArray[1]);
+            if (seconds === 0 && minutes === 0) {
+                clearInterval(intervalId);
+            } else if (seconds === 0) {
+                minutes--;
+                seconds = 59;
+            } else {
+                seconds--;
+            }
+            let newTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+            document.querySelector("#timer").value = newTime;
+        }, 1000);
+    });
+}
+
+//Checks if the stop button is clicked, if so, it stops the timer
+function stopButtonAttacher() {
+    let button = document.querySelector("#stop");
+    button.addEventListener("click", () => {
+        clearInterval(intervalId);
+    });
+}
+
+//What to run when the window is loaded
+window.onload = function () {
+    startButtonAttacher();
+    stopButtonAttacher();
+}
