b65d2016324b7ba3c011b1d83034d4e7a1a4dc00
Lily Cholmondeley <lily.cholmo1@gmail.com>
2024-04-05 02:49:28 +0100

    Changed from migration file to JS to make DB

--- setup.mjs
+++ setup.mjs
@@ -1,13 +1,56 @@
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
-async function init() {
+export async function init() {
   const db = await open({
     filename: './database.sqlite',
     driver: sqlite3.Database,
     verbose: true,
   });
-  await db.migrate({ migrationsPath: './migration-sqlite/' });
+  await db.run('CREATE TABLE workouts (id INTEGER PRIMARY KEY, data TEXT NOT NULL, user CHAR(36), time DATETIME)');
+  await db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT');
+  await db.run('INSERT INTO users (name) VALUES ("Lily"), ("Mark"), "Matt"');
+  const workoutData = [{
+    id: '1',
+    name: 'Workout 1',
+    description: 'Feebly doo feebly da',
+    timings: [
+      '5:00',
+      '1:00',
+      '3:00',
+      '1:00',
+      '4:00',
+    ],
+    exercises: [
+      'Jumping Jacks',
+      'Rest',
+      'Pushups',
+      'Rest',
+      'Squats',
+    ],
+  },
+  {
+    id: '2',
+    name: 'Workout 2',
+    description: 'Feebly doo feebly da',
+    timings: [
+      '5:00',
+      '1:00',
+      '3:00',
+      '1:00',
+      '4:00',
+    ],
+    exercises: [
+      'Jumping Jacks',
+      'Rest',
+      'Pushups',
+      'Rest',
+      'Squats',
+    ],
+  }];
+  for (const workout of workoutData) {
+    await db.run('INSERT INTO workouts (data, user) VALUES (?, ?)', JSON.stringify(workout), 1);
+  }
   return db;
 }
 
